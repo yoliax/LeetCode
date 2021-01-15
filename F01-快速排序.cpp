9d855aa30638fc1b085c8829b5448c4451adae4f
yoliax <yoliax@163.com>
2021-01-15 14:16:07 +0800

    quick-sort

--- F01-快速排序.cpp
+++ F01-快速排序.cpp
@@ -1,4 +1,5 @@
 #include <vector>
+#include <iostream>
 
 using namespace std;
 
@@ -7,7 +8,24 @@ void quickSort(vector<int> &nums, int l, int r) {
         return;
     int first = l, last = r - 1, key = nums[first];
     while (first < last) {
-
+        while (first < last && nums[last] >= key) {
+            --last;
+        }
+        nums[first] = nums[last];
+        while (first < last && nums[first] <= key) {
+            ++first;
+        }
+        nums[last] = nums[first];
     }
+    nums[first] = key;
+    quickSort(nums, l, first);
+    quickSort(nums, first + 1, r);
 }
 
+int main() {
+    vector<int> nums = {6, 1, 2, 7, 9, 3, 4, 5, 10, 8};
+    quickSort(nums, 0, nums.size());
+    for(auto x: nums){
+        cout << x << endl;
+    }
+}
