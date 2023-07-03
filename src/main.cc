00e5b7ba54080ef189e3316c4e9cce3c851f8eac
yzh <yoliax@163.com>
2023-07-03 17:18:38 +0800

    454

---
+++ src/main.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+struct ListNode
+{
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+};
+
+
+int main(int argc, char const *argv[])
+{
+  std::cout << "hello" << std::endl;
+  return 0;
+}
